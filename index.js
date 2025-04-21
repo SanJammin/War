1c73ba1990e49cd7a7e3d101966808526df8ce06
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-04-21 21:12:59 +0100

    Create ability to draw card

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+let deckId;
+
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            deckId = data.deck_id;
+        })
+};
+
+function drawNewCard() {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => console.log(data))
 };
 
 document.getElementById("new-deck").addEventListener("click", handleClick);
+document.getElementById("draw-card").addEventListener("click", drawNewCard);
